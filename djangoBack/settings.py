e19789409ee6b6657b2f203d564be0cbfe2646a0
unknown <juanestebanmosquera32@gmail.com>
2024-05-06 16:44:25 -0500

    variables de entorno y git ignore

--- djangoBack/settings.py
+++ djangoBack/settings.py
@@ -1,3 +1,6 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
 """
 Django settings for djangoBack project.
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8=kvu+b2es7=@suf^!3n!wo+@#1gp5^93m+nc7^!_ng+vn@q$*'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
